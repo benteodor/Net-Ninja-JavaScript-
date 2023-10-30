64c560d026ceb57b56da198415d82bd5a160e2b1
unknown <teodor.popa96@gmail.com>
2023-10-30 09:53:05 +0200

    If statements

--- app.js
+++ app.js
@@ -3,3 +3,17 @@ alert("Hello, world");
 
 // the second alert
 alert("Welcome back!");
+
+var youLikeMeat = true;
+
+if (youLikeMeat) {
+  document.write("Here is the meat menu");
+}
+
+var myNum = 8;
+
+if (myNum == 10) {
+  document.write("myNum is equal to 10");
+} else {
+  document.write("myNum is not equal to 10");
+}

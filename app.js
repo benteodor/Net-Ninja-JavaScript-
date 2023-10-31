84c1897fb38af3b5b7ddb74189b3a03b66ba6b22
unknown <teodor.popa96@gmail.com>
2023-10-31 09:42:20 +0200

    While loops

--- app.js
+++ app.js
@@ -37,3 +37,11 @@ if (yourAge >= 18 && yourAge <= 30) {
 } else {
   document.write("You can not join!");
 }
+
+var age = 5;
+
+while (age < 10) {
+  console.log("Your age is less than 10");
+  age++;
+}
+document.write("You are now over 10");

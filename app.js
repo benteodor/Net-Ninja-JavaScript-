e70160d53023cdac2aed46107795652b397bc316
unknown <teodor.popa96@gmail.com>
2023-11-02 09:17:47 +0200

    Update Continue

--- app.js
+++ app.js
@@ -67,8 +67,12 @@ document.write("all links now looped");
 // Break & Continue
 
 for (i = 0; i < 10; i++) {
+  if (i === 5 || i === 3) {
+    continue;
+  }
   console.log(i);
   if (i == 7) {
     break;
   }
 }
+console.log("I have broken out of the loop");

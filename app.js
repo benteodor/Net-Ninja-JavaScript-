102bf876174269bed54060cdd66a4632b0dd71be
unknown <teodor.popa96@gmail.com>
2023-11-22 21:23:36 +0200

    JS Update

--- app.js
+++ app.js
@@ -273,4 +273,11 @@ const likes = 30;
 // template string way
 let result = `The blog called ${title} by ${author} has ${likes} likes`;
 console.log(result);
+
 // creating html templates
+/*let html = 
+<h2>${title}</h2>
+<p>By ${author}</p>
+<span>This blog has ${likes} likes</span>
+
+console.log(html);*/

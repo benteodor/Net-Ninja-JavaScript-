39f7d28de93d175af8a76ea6182b8c845806d97f
unknown <teodor.popa96@gmail.com>
2023-11-23 19:55:13 +0200

    JS

--- app.js
+++ app.js
@@ -285,3 +285,9 @@ console.log(html);*/
 let ninjas = ["shaun", "ryu", "chun-li"];
 ninjas[1] = "ken";
 console.log(ninjas[1]);
+
+let ages = [20, 25, 30, 35];
+console.log(ages[2]);
+
+let random = ["shaun", "crystal", 30, 20];
+console.log(random);
